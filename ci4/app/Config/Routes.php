f583ff71924a650af699eec3bee95b589e5dbe2d
Mulifatkhul Rachiim <mulifatkhul.rach2@gmail.com>
2022-06-19 21:20:50 +0700

    1

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -21,7 +21,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-$routes->setAutoRoute(false);
+
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
@@ -37,10 +37,10 @@ $routes->setAutoRoute(false);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('/about', 'page::about');
-$routes->get('/contact', 'page::contact');
-$routes->get('faqs', 'page::faqs');
-
+$routes->get('/about', 'Page::about');
+$routes->get('/kontak', 'Page::kontak');
+$routes->get('/artikel', 'Page::artikel');
+$routes->get('/tos', 'Page::tos');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
--- ci4/app/Controllers/page.php
+++ ci4/app/Controllers/page.php
@@ -1,21 +1,32 @@
 <?php
-
-namespace App\controllers;
-
+namespace App\Controllers;
+use App\Models\ArtikelModel;
 class Page extends BaseController
 {
     public function about()
     {
-        echo "Ini halaman About";
+        return view('about', [
+            'title' => '',
+            'content' => ''
+            ]);
+    }
+    public function kontak()
+    {
+        return view('kontak', [
+            'title' => '',
+            'content' => ''
+            ]);
+    }
+    public function artikel()
+    {
+        $title = 'Daftar Artikel';
+        $model = new ArtikelModel();
+        $artikel = $model->findAll();
+        return view('artikel/index', compact('artikel', 'title'));
+    }
+    public function tos()
+    {
+    echo "ini halaman Term of Services";
     }
 
-    public function contact()
-    {
-        echo "ini halaman Contact";
-    }
-
-    public function faqs()
-    {
-        echo "Ini halaman FAQ";
-    }
 }
